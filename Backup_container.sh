65bb5cc1129897a9fcd5ddf80ec8e3deb421c6aa
Ali Taheri <107788739+alitaheri1985@users.noreply.github.com>
2024-11-19 13:44:17 +0330

    Update Backup_container.sh
    Some changes

--- Backup_container.sh
+++ Backup_container.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-`docker container ls --format "table {{.Names}}" | tail -n +2 > /root/containers/containers_running.txt`
+mkdir -p /root/containers
 
-`chmod 755 /root/containers/containers_running.txt`
+CONTAINER=/root/containers
 
-for line in $(cat /root/containers/containers_running.txt);do
+`docker container ls --format "table {{.Names}}" | tail -n +2 > $CONTAINER/containers_running.txt`
 
-        docker export -o /root/containers/backup-`date +%Y-%m-%d`_$line.tar $line
+`chmod 755 $CONTAINER/containers_running.txt`
+
+for line in $(cat $CONTAINER/containers_running.txt);do
+
+        docker export -o $CONTAINER/backup-`date +%Y-%m-%d`_$line.tar $line
 
         if [ $? = 0 ]; then
 
